6672bf229c01ad97dc81021fd9d0b3b988b28fca
GustavoMoralesGa <gustavo.morales.ga@gmail.com>
2022-10-27 23:27:16 -0300

    [add] Header

---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import Card from '../components/Card';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Card />
+    </>
+  )
+}
+
+export default Home; 
