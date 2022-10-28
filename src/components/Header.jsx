38916b7f05b32e7f0b33083b8dfc8c58bc96e7c0
GustavoMoralesGa <gustavo.morales.ga@gmail.com>
2022-10-27 23:39:10 -0300

    [add] Header Base

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import '../css/index.css'
+
+export default function Header() {
+  return (
+    <div className='text-center rounded-3 header'>
+      <h1 className='title'>¡Pizzeria Mamma Mía!</h1>
+      <h3 className='subTitle'>¡tenemos las mejores pizzas del mundo mundial!</h3>
+    </div>
+  )
+}
